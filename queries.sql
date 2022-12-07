1e355c2c374e632e62202ecdd94646676c25c16a
Lucas Comamala <lucascomamala@gmail.com>
2022-12-07 09:37:14 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
+
 /* Day 1 */
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
@@ -8,3 +9,32 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered IS true;
 SELECT * from animals WHERE name NOT IN ('Gabumon');
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Day 2 */
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS null;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT sp1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO sp1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM ANIMALS WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) AS average_weight from ANIMALS;
+SELECT SUM(escape_attempts), neutered FROM animals GROUP BY neutered;
+SELECT MAX(weight_kg), MIN(weight_kg), species FROM animals GROUP BY species;
+SELECT AVG(escape_attempts), species FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
